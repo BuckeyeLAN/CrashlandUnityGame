ef68d53f6582e8f953238963736523f9acca3e12
gpuhalla <gpuhalla1@gmail.com>
2016-11-19 17:23:30 -0500

    Updated Lists+ shuffle

--- Assets/DisasterDeck.cs
+++ Assets/DisasterDeck.cs
@@ -1,18 +1,28 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class DisasterDeck {
 
 
-    private string[] DisasterStringArray = new string[] { "Sudden Spoiling", "Suit Leak", "Acid Rain", "Inventory Loss", "Artifact Lost", "Ship Ravaged", "Inventory Loss", "Tragic Failure", "Lose Hope", "Completely Lost", "Catastrophic Failure", "Bad Salvage", "Misplaced Resources", "Catastrophic Failure", "Bad Salvage" };
+    private List<string> DisasterStringList = new List<string>(new string[] {"Sudden Spoiling", "Suit Leak", "Acid Rain", "Inventory Loss", "Artifact Lost", "Ship Ravaged", "Inventory Loss", "Tragic Failure", "Lose Hope", "Completely Lost", "Catastrophic Failure", "Bad Salvage", "Misplaced Resources", "Catastrophic Failure", "Bad Salvage" });
+
 
     // Use this for initialization
     public DisasterDeck () {
         
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public void Shuffle ()
+    {
+        int n = DisasterStringList.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = (int)(Random.Range(0, DisasterStringList.Count));
+            string value = DisasterStringList[k];
+            DisasterStringList[k] = DisasterStringList[n];
+            DisasterStringList[n] = value;
+        }
+    }
+
 }
