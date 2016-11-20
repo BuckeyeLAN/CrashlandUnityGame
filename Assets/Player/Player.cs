b4593f5b5361f235edeeb29e3698c8b33eb2fb22
Lucas Duncan <ljdduncan@gmail.com>
2016-11-19 19:01:22 -0500

    added players and ship

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour {
+
+    public int food = 0;
+    public int oxygen = 0;
+    public int scrap = 0;
+    public List<string> inventory = new List<string>();
+
+    public int[] location = new int[] { 0, 0 };
+
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void addFood(int num)
+    {
+        food += num;
+    }
+    void addOxygen(int num)
+    {
+        oxygen += num;
+    }
+    void addScrap(int num)
+    {
+        scrap += num;
+    }
+    void addItem(DiscoveryCard discovery)
+    {
+        inventory.Add(discovery);
+    }
+}
---
+++ Assets/Ship.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Ship : MonoBehaviour {
+
+    public int food = 0;
+    public int oxygen = 0;
+    public int scrap = 0;
+    public List<string> inventory = new List<string>();
+    public int coreParts = 0;
+    public bool teleporterAssembled = false;
+    public bool aiCoreAssembled = false;
+    public bool transposerAssembled = false;
+    public bool cryopodsAssembled = false;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
