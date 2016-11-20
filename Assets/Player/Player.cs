d3db1105eb900c7781aa971bac583d816dfcb1c7
Lucas Duncan <ljdduncan@gmail.com>
2016-11-19 19:50:07 -0500

    player and ship added

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -38,4 +38,8 @@ public class Player : MonoBehaviour {
     {
         inventory.Add(discovery);
     }
+    void removeItem(DiscoveryCard discovery)
+    {
+        inventory.Remove(discovery);
+    }
 }
--- Assets/Ship.cs
+++ Assets/Ship.cs
@@ -20,6 +20,27 @@ public class Ship : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+    }
+
+    void addFood(int num)
+    {
+        food += num;
+    }
+    void addOxygen(int num)
+    {
+        oxygen += num;
+    }
+    void addScrap(int num)
+    {
+        scrap += num;
+    }
+    void addItem(DiscoveryCard discovery)
+    {
+        inventory.Add(discovery);
+    }
+    void removeItem(DiscoveryCard discovery)
+    {
+        inventory.Remove(discovery);
+    }
 }
